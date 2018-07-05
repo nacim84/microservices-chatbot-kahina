56634f99e13b2f19687c1070b5a053511e4d8499
PAR-N-PW07-4920\admnrabi <nacim.rabia@businessdecision.com>
2018-07-05 15:29:25 +0200

    Initial application generated by JHipster-5.0.2

---
+++ src/main/java/com/chatbot/kahina/config/FeignConfiguration.java
@@ -0,0 +1,18 @@
+package com.chatbot.kahina.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.chatbot.kahina")
+public class FeignConfiguration {
+
+    /**
+     * Set the Feign specific log level to log client REST requests
+     */
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.BASIC;
+    }
+}
---
+++ src/main/java/com/chatbot/kahina/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.chatbot.kahina.web.rest;
